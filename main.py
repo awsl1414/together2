4b11fcb06974ec2909ab2420e96669d5e0a7a04a
awsl1414 <3030994569@qq.com>
2023-05-31 21:21:20 +0800

    做了点小修改

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 from uvicorn import run
 from crud import app01
@@ -11,6 +12,15 @@ register_tortoise(
     modules={"models": ["models"]},
     generate_schemas=True,
     add_exception_handlers=True,
+)
+
+# 中间件配置
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许的域名，["*"]表示允许所有
+    allow_credentials=True,  # 是否允许发送 Cookie
+    allow_methods=["*"],  # 允许的 HTTP 方法；
+    allow_headers=["*"],  # 允许的请求 Header
 )
 
 if __name__ == "__main__":
