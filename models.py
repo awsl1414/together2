f23664ebf3816439e42814bd9c42958e18172129
awsl1414 <95275520+awsl1414@users.noreply.github.com>
2023-06-01 12:15:29 +0800

    Update models.py

--- models.py
+++ models.py
@@ -5,4 +5,4 @@ from tortoise import fields
 class Info(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=32, null=False, descrrption="姓名")
-    tel = fields.CharField(max_length=64, null=False, description="电话")
+    tel = fields.CharField(max_length=64, null=False, unique=True, description="电话")
